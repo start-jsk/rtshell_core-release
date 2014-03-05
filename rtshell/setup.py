b72d9666c010084a0c511913d3eb046256bb64e1
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-03-05 17:56:21 +0900

    Revert "Trimmed the branch to only the rtshell_core sub directory"
    This reverts commit 16039f0db48e8f7c7bed1e427509eb66a6256885.

---
+++ rtshell/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['rtshell'],
+    package_dir={'': 'lib/python2.7/site-packages'}
+)
+
+setup(**d)
